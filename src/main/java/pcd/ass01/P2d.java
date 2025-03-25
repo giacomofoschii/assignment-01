d59d14c02b9ca6160762e0caf898b92a8cb86cac
Giacomo Foschi <giacomo.foschi3@studio.unibo.it>
2025-03-25 09:49:31 +0100

    Working with jpf

--- src/main/java/pcd/ass01/P2d.java
+++ src/main/java/pcd/ass01/P2d.java
@@ -6,26 +6,52 @@ package pcd.ass01;
  * objects are completely state-less
  *
  */
-public record P2d(double x, double y) {
+public class P2d {
+    private final double x;
+    private final double y;
 
-
-    public P2d sum(V2d v){
-        return new P2d(x+v.x(),y+v.y());
+    public P2d(double x, double y) {
+        this.x = x;
+        this.y = y;
     }
 
-    public V2d sub(P2d v){
-        return new V2d(x-v.x,y-v.y);
+    public double x() {
+        return x;
+    }
+
+    public double y() {
+        return y;
+    }
+
+    public P2d sum(V2d v) {
+        return new P2d(x + v.x(), y + v.y());
+    }
+
+    public V2d sub(P2d v) {
+        return new V2d(x - v.x(), y - v.y());
     }
     
     public double distance(P2d p) {
-    	double dx = p.x - x;
-    	double dy = p.y - y;
+        double dx = p.x() - x;
+        double dy = p.y() - y;
     	return Math.sqrt(dx*dx + dy*dy);
-
     }
-    
-    public String toString(){
-        return "P2d("+x+","+y+")";
+
+    public String toString() {
+        return "P2d(" + x + "," + y + ")";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        P2d p2d = (P2d) obj;
+        return Double.compare(p2d.x, x) == 0 && Double.compare(p2d.y, y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * Double.hashCode(x) + Double.hashCode(y);
     }
 
 }
--- src/main/java/pcd/ass01/V2d.java
+++ src/main/java/pcd/ass01/V2d.java
@@ -15,27 +15,55 @@ package pcd.ass01;
  * objects are completely state-less
  *
  */
-public record V2d(double x,double y) {
+public class V2d {
+    private final double x;
+    private final double y;
 
-    public V2d sum(V2d v){
-        return new V2d(x+v.x,y+v.y);
+    public V2d(double x, double y) {
+        this.x = x;
+        this.y = y;
     }
 
-    public double abs(){
-        return (double)Math.sqrt(x*x+y*y);
+    public double x() {
+        return x;
     }
 
-    public V2d getNormalized(){
-        double module=(double)Math.sqrt(x*x+y*y);
-        return new V2d(x/module,y/module);
+    public double y() {
+        return y;
     }
 
-    public V2d mul(double fact){
-        return new V2d(x*fact,y*fact);
+    public V2d sum(V2d v) {
+        return new V2d(x + v.x(), y + v.y());
     }
 
-    public String toString(){
-        return "V2d("+x+","+y+")";
+    public double abs() {
+        return Math.sqrt(x * x + y * y);
     }
-    
+
+    public V2d getNormalized() {
+        double module = Math.sqrt(x * x + y * y);
+        return new V2d(x / module, y / module);
+    }
+
+    public V2d mul(double fact) {
+        return new V2d(x * fact, y * fact);
+    }
+
+    @Override
+    public String toString() {
+        return "V2d(" + x + "," + y + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        V2d v2d = (V2d) o;
+        return Double.compare(v2d.x, x) == 0 && Double.compare(v2d.y, y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Double.hashCode(x) * 31 + Double.hashCode(y);
+    }
 }
