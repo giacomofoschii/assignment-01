54a32ba8b2d46e4aab0a7b22f017472c96219719
Giovanni <giovanni.pisoni@studio.unibo.it>
2025-03-28 13:19:58 +0100

    Revert "[MULTITHREAD] moved stopandRestart in controller"
    This reverts commit 63154cfd22af9f8624a6fcaaee85aa3e7a51114f.

--- src/main/java/pcd/ass01/multithreading/BoidsView.java
+++ src/main/java/pcd/ass01/multithreading/BoidsView.java
@@ -3,58 +3,95 @@ package pcd.ass01.multithreading;
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+
 import java.awt.*;
 import java.util.Hashtable;
 
 public class BoidsView implements ChangeListener {
 
-    private final JFrame frame;
-    private final BoidsPanel boidsPanel;
-    private final JSlider cohesionSlider, separationSlider, alignmentSlider;
-    private final JButton stopButton, pauseButton;
-    private boolean isRunning;
-    private final BoidsModel model;
-    private final BoidsController boidsController;
-    private final int width, height;
+	private final JFrame frame;
+	private final BoidsPanel boidsPanel;
+	private final JSlider cohesionSlider, separationSlider, alignmentSlider;
+	private final JButton stopButton, pauseButton;
+	private boolean isRunning;
+	private final BoidsModel model;
+	private final BoidsController boidsController;
+	private final int width, height;
+	
+	public BoidsView(final BoidsModel model, final int width, final int height) {
+		this.model = model;
+		this.width = width;
+		this.height = height;
+		this.isRunning = true;
+		this.boidsController = new BoidsController();
 
-    public BoidsView(final BoidsModel model, final int width, final int height) {
-        this.model = model;
-        this.width = width;
-        this.height = height;
-        this.isRunning = true;
-        this.boidsController = new BoidsController();
-
-        frame = new JFrame("Boids Simulation");
+		frame = new JFrame("Boids Simulation");
         frame.setSize(width, height);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JPanel cp = new JPanel();
-        cp.setLayout(new BorderLayout());
+		JPanel cp = new JPanel();
+		LayoutManager layout = new BorderLayout();
+		cp.setLayout(layout);
 
         boidsPanel = new BoidsPanel(this, model);
-        cp.add(BorderLayout.CENTER, boidsPanel);
+		cp.add(BorderLayout.CENTER, boidsPanel);
 
-        JPanel controlPanel = new JPanel();
+		JPanel controlPanel = new JPanel();
 
-        pauseButton = new JButton("Pause");
-        stopButton = new JButton("Stop");
+		pauseButton = new JButton("Pause");
+		stopButton = new JButton("Stop");
 
-        stopButton.addActionListener(e -> boidsController.stopAndRestartSimulation(this, this.model));
-
-        pauseButton.addActionListener(e -> {
-            if (isRunning) {
-                boidsController.getSimulator().pauseSimulation();
-                pauseButton.setText("Resume");
-            } else {
-                boidsController.getSimulator().resumeSimulation();
-                pauseButton.setText("Pause");
-            }
-            isRunning = !isRunning;
+		stopButton.addActionListener(e -> {
+			this.boidsController.getSimulator().stopSimulation();  // Ferma la simulazione attuale
+		
+			boolean restarting = false;
+			while (!restarting) {
+				// Mostra il dialogo di input per il numero di boids
+				String input = JOptionPane.showInputDialog(frame, "Inserisci il numero di boids:",
+						"Numero di Boids", JOptionPane.QUESTION_MESSAGE);
+		
+				if (input == null) { 
+					frame.dispose(); 
+					System.exit(0); // Se l'utente annulla, chiude tutto
+				}
+		
+				try {
+					int nBoids = Integer.parseInt(input);
+					if (nBoids > 0) {
+						this.boidsController.setBoidsNumber(nBoids, this.model); // Imposta il numero di boids
+						this.boidsController.initializeBoidsSimulator(this.model); // Crea un nuovo simulatore
+						this.boidsController.getSimulator().attachView(this);
+		
+						// Avvia la nuova simulazione in un thread separato
+						new Thread(this.boidsController.getSimulator()::runSimulation).start();
+		
+						restarting = true;
+					} else {
+						JOptionPane.showMessageDialog(frame, "Il numero di boids deve essere positivo",
+								"Errore di input", JOptionPane.ERROR_MESSAGE);
+					}
+				} catch (NumberFormatException ex) {
+					JOptionPane.showMessageDialog(frame, "Inserisci un numero valido",
+							"Errore di input", JOptionPane.ERROR_MESSAGE);
+				}
+			}
         });
 
-        controlPanel.add(stopButton);
-        controlPanel.add(pauseButton);
-        cp.add(BorderLayout.NORTH, controlPanel);
+		pauseButton.addActionListener(e -> {
+			if (isRunning) {
+				this.boidsController.getSimulator().pauseSimulation();
+				pauseButton.setText("Resume");
+			} else {
+				this.boidsController.getSimulator().resumeSimulation();
+				pauseButton.setText("Pause");
+			}
+			isRunning = !isRunning;
+		});
+
+		controlPanel.add(stopButton);
+		controlPanel.add(pauseButton);
+
+		cp.add(BorderLayout.NORTH, controlPanel);
 
         JPanel slidersPanel = new JPanel();
 
@@ -69,79 +106,85 @@ public class BoidsView implements ChangeListener {
         slidersPanel.add(new JLabel("Cohesion"));
         slidersPanel.add(cohesionSlider);
 
-        cp.add(BorderLayout.SOUTH, slidersPanel);
+		cp.add(BorderLayout.SOUTH, slidersPanel);
 
-        frame.setContentPane(cp);
+		frame.setContentPane(cp);
+
         frame.setVisible(true);
 
-        startPanel();
-    }
+		startPanel();
+	}
 
-    private void startPanel() {
-        boolean starting = false;
-        while (!starting) {
-            String input = JOptionPane.showInputDialog(frame, "Inserisci il numero di boids:",
-                    "Numero di Boids", JOptionPane.QUESTION_MESSAGE);
-            if (input == null) {
-                frame.dispose();
-                System.exit(0);
-            }
-            try {
-                int nBoids = Integer.parseInt(input);
-                if (nBoids > 0) {
-                    model.setBoidsNumber(nBoids);
-                    starting = true;
-                } else {
-                    JOptionPane.showMessageDialog(frame, "Il numero di boids deve essere positivo",
-                            "Errore di input", JOptionPane.ERROR_MESSAGE);
-                }
-            } catch (NumberFormatException ex) {
-                JOptionPane.showMessageDialog(frame, "Inserisci un numero valido",
-                        "Errore di input", JOptionPane.ERROR_MESSAGE);
-            }
-        }
-    }
+	private void startPanel() {
+		boolean starting = false;
+		while(!starting) {
+			// Mostra il dialogo di input per il numero di boids
+			String input = JOptionPane.showInputDialog(frame, "Inserisci il numero di boids:",
+					"Numero di Boids", JOptionPane.QUESTION_MESSAGE);
+			if(input == null) {
+				frame.dispose();
+				System.exit(0);
+			}
+			try {
+				int nBoids = Integer.parseInt(input);
+				if (nBoids > 0) {
+					model.setBoidsNumber(nBoids);
+					starting = true;
+				} else {
+					JOptionPane.showMessageDialog(frame, "Boids' number must be positive",
+							"Errore di input", JOptionPane.ERROR_MESSAGE);
+				}
+			} catch (NumberFormatException ex) {
+				JOptionPane.showMessageDialog(frame, "Letters or symbols not allowed, insert a valid number!",
+						"Errore di input", JOptionPane.ERROR_MESSAGE);
+			}
+		}
+	}
 
-    private JSlider makeSlider() {
-        var slider = new JSlider(JSlider.HORIZONTAL, 0, 20, 10);
-        slider.setMajorTickSpacing(10);
-        slider.setMinorTickSpacing(1);
-        slider.setPaintTicks(true);
-        slider.setPaintLabels(true);
-        Hashtable<Integer, JLabel> labelTable = new Hashtable<>();
-        labelTable.put(0, new JLabel("0"));
-        labelTable.put(10, new JLabel("1"));
-        labelTable.put(20, new JLabel("2"));
-        slider.setLabelTable(labelTable);
+	private JSlider makeSlider() {
+		var slider = new JSlider(JSlider.HORIZONTAL, 0, 20, 10);        
+		slider.setMajorTickSpacing(10);
+		slider.setMinorTickSpacing(1);
+		slider.setPaintTicks(true);
+		slider.setPaintLabels(true);
+		Hashtable labelTable = new Hashtable<>();
+		labelTable.put( 0, new JLabel("0") );
+		labelTable.put( 10, new JLabel("1") );
+		labelTable.put( 20, new JLabel("2") );
+		slider.setLabelTable( labelTable );
+		slider.setPaintLabels(true);
         slider.addChangeListener(this);
-        return slider;
-    }
+		return slider;
+	}
+	
+	public void update(int frameRate) {
+		boidsPanel.setFrameRate(frameRate);
+		boidsPanel.repaint();
+	}
 
-    public void update(int frameRate) {
-        boidsPanel.setFrameRate(frameRate);
-        boidsPanel.repaint();
-    }
+	@Override
+	public void stateChanged(ChangeEvent e) {
+		if (e.getSource() == separationSlider) {
+			var val = separationSlider.getValue();
+			model.setSeparationWeight(0.1*val);
+		} else if (e.getSource() == cohesionSlider) {
+			var val = cohesionSlider.getValue();
+			model.setCohesionWeight(0.1*val);
+		} else {
+			var val = alignmentSlider.getValue();
+			model.setAlignmentWeight(0.1*val);
+		}
+	}
+	
+	public int getWidth() {
+		return width;
+	}
 
-    @Override
-    public void stateChanged(ChangeEvent e) {
-        if (e.getSource() == separationSlider) {
-            model.setSeparationWeight(0.1 * separationSlider.getValue());
-        } else if (e.getSource() == cohesionSlider) {
-            model.setCohesionWeight(0.1 * cohesionSlider.getValue());
-        } else {
-            model.setAlignmentWeight(0.1 * alignmentSlider.getValue());
-        }
-    }
+	public int getHeight() {
+		return height;
+	}
 
-    public int getWidth() {
-        return width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void setSimulator(BoidsSimulator simulator) {
-        this.boidsController.setBoidsSimulator(simulator);
-    }
+	public void setSimulator(BoidsSimulator simulator) {
+		this.boidsController.setBoidsSimulator(simulator);
+	}
 }
