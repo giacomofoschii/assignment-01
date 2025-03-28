63154cfd22af9f8624a6fcaaee85aa3e7a51114f
gioele santi <93931915+Saint138@users.noreply.github.com>
2025-03-28 12:42:37 +0100

    [MULTITHREAD] moved stopandRestart in controller

--- src/main/java/pcd/ass01/multithreading/BoidsController.java
+++ src/main/java/pcd/ass01/multithreading/BoidsController.java
@@ -1,8 +1,10 @@
 package pcd.ass01.multithreading;
 
+import javax.swing.*;
+
 public class BoidsController {
 
-    BoidsSimulator boidsSimulator;
+    private BoidsSimulator boidsSimulator;
 
     public void setBoidsNumber(final int nBoids, final BoidsModel model) {
         model.setBoidsNumber(nBoids);
@@ -20,4 +22,36 @@ public class BoidsController {
         return this.boidsSimulator;
     }
 
+    public void stopAndRestartSimulation(BoidsView view, BoidsModel model) {
+        if (boidsSimulator != null) {
+            boidsSimulator.stopSimulation();
+        }
+
+        boolean restarting = false;
+        while (!restarting) {
+            String input = JOptionPane.showInputDialog(null, "Inserisci il numero di boids:", 
+                    "Numero di Boids", JOptionPane.QUESTION_MESSAGE);
+
+            if (input == null) {
+                System.exit(0);
+            }
+
+            try {
+                int nBoids = Integer.parseInt(input);
+                if (nBoids > 0) {
+                    setBoidsNumber(nBoids, model);
+                    initializeBoidsSimulator(model);
+                    boidsSimulator.attachView(view);
+                    new Thread(boidsSimulator::runSimulation).start();
+                    restarting = true;
+                } else {
+                    JOptionPane.showMessageDialog(null, "Il numero di boids deve essere positivo",
+                            "Errore di input", JOptionPane.ERROR_MESSAGE);
+                }
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(null, "Inserisci un numero valido",
+                        "Errore di input", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
 }
