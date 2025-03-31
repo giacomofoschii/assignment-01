249ac8d4aba4a7235416a45bec284a936f46a8c2
Giovanni <giovanni.pisoni@studio.unibo.it>
2025-03-31 13:43:23 +0200

    [VIRTUAL-THREAD] Updated VirtualThreads_develop branch, copied the main's template

--- src/main/java/pcd/ass01/multithreading/jpftesting/JPFBoidsSimulation.java
+++
@@ -1,9 +0,0 @@
-package pcd.ass01.multithreading.jpftesting;
-
-public class JPFBoidsSimulation {
-
-    public static void main(String[] args) {
-        var boidsController = new JPFBoidsController();
-        boidsController.initialize();
-    }
-}
