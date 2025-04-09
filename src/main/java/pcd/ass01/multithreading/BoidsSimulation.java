c47207739838bceeef198c90993bfc0f1e75cba3
Giacomo Foschi <giacomo.foschi3@studio.unibo.it>
2025-04-09 15:26:44 +0200

    [PROJECT] Refactoring for delivery

--- src/main/java/pcd/ass01/multithreading/BoidsSimulation.java
+++ src/main/java/pcd/ass01/multithreading/BoidsSimulation.java
@@ -1,5 +1,12 @@
 package pcd.ass01.multithreading;
 
+/*
+    Author:
+    - Giacomo Foschi (giacomo.foschi2@studio.unibo.it)
+    - Giovanni Pisoni (giovanni.pisoni@studio.unibo.it)
+    - Giovanni Rinchiuso (giovanni.rinchiuso@studio.unibo.it)
+    - Gioele Santi (gioele.santi2@studio.unibo.it)
+ */
 public class BoidsSimulation {
 
     public static void main(String[] args) {
--- src/main/java/pcd/ass01/multithreading/jpftesting/JPFBoidsSimulation.java
+++ src/main/java/pcd/ass01/multithreading/jpftesting/JPFBoidsSimulation.java
@@ -1,5 +1,12 @@
 package pcd.ass01.multithreading.jpftesting;
 
+/*
+    Author:
+    - Giacomo Foschi (giacomo.foschi2@studio.unibo.it)
+    - Giovanni Pisoni (giovanni.pisoni@studio.unibo.it)
+    - Giovanni Rinchiuso (giovanni.rinchiuso@studio.unibo.it)
+    - Gioele Santi (gioele.santi2@studio.unibo.it)
+ */
 public class JPFBoidsSimulation {
 
     public static void main(String[] args) {
